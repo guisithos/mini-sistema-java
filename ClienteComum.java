4e91842a4e58cc1756de4d456ec5b5486ed29aea
Guilherme Thomas <gui.thomas@outlook.com>
2021-10-31 15:59:10 -0300

    Update ClienteComum.java

--- ClienteComum.java
+++ ClienteComum.java
@@ -1,9 +1,4 @@
-/**
- * Write a description of class ClienteComum here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class ClienteComum extends Cliente
 {
     public ClienteComum(String n, String e, int i) {
