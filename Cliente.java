3a8eb730d529d8c57a69e3521d42822790860873
Guilherme Thomas <gui.thomas@outlook.com>
2021-10-31 15:59:32 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,38 +1,25 @@
-/**
- * Classe que contém as informações dos clientes
- * como "nome", "email", "idade", etc. 
- * <br />
- * Possui métodos para ajustar e obter esses dados.
- * 
- * @author (Alfamidia) 
- * @version (1.0)
- */
+
 public abstract class Cliente
 {
     private String nome;
     private String email;
     private int idade;
     
-    /**
-     * Construtor com inicialização básica
-     */
+   
     public Cliente() {
         this.nome = "";
         this.email = "";
         this.idade = 0;
     }
     
-    /**
-     * Construtor 
-     * @param (String, String, int)
-     */
+    
     public Cliente(String nome, String email, int idade) {
         this.nome = nome;
         this.email = email;
         this.idade = idade;
     }
 
-    //métodos "get" e "set"
+    //get e set
     public String retornarNome() {
         return this.nome;
     }
