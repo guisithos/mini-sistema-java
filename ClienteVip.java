e0a83421fef6ac8ebf44c9f2431e5253a6122140
Guilherme Thomas <gui.thomas@outlook.com>
2021-10-31 15:58:44 -0300

    Update ClienteVip.java

--- ClienteVip.java
+++ ClienteVip.java
@@ -1,12 +1,7 @@
-/**
- * Write a description of class ClienteVip here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class ClienteVip extends Cliente
 {
-    //variável que contém o limite do cheque especial
+    //limite do cliente vip
     double limite = 3000;
     
     public ClienteVip(String n, String e, int i) {
